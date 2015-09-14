fe313530d8c85625d3c9f4bee055208a79587032
andy <andy@freilandkiwis.de>
2015-09-14 20:56:06 +0200

    use concrete paths

--- vintage/wsgi.py
+++ vintage/wsgi.py
@@ -15,11 +15,11 @@ from django.core.wsgi import get_wsgi_application
 
 
 # Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('~/.virtualenvs/vintage/local/lib/python2.7/site-packages')
+site.addsitedir('/home/andy/.virtualenvs/vintage/local/lib/python2.7/site-packages')
 
 # Add the app's directory to the PYTHONPATH
-sys.path.append('~/www/vintage')
-sys.path.append('~/www/vintage/vintage')
+sys.path.append('/home/andy/www/vintage')
+sys.path.append('/home/andy/www/vintage/vintage')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
